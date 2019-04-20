431d91fe656cf7644d9956afe4aecac9a4bbdff6
alauda <alauda.jacx@gmail.com>
2019-04-20 23:48:57 +0900

    tenka1

---
+++ tenka1/tenka1_pb_2019/cmake-build-debug/CMakeFiles/tenka1_pb_2019a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tenka1_pb_2019a.dir/q1001.cpp.obj"
+  "tenka1_pb_2019a.pdb"
+  "tenka1_pb_2019a.exe"
+  "tenka1_pb_2019a.exe.manifest"
+  "libtenka1_pb_2019a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tenka1_pb_2019a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
