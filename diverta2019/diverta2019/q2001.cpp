d472ec8565c370ec78f6918b88f8c6dfc9c55770
alauda <alauda.jacx@gmail.com>
2019-05-11 23:37:09 +0900

    diverta2019

--- diverta2019/diverta2019/q2001.cpp
+++ diverta2019/diverta2019/q2001.cpp
@@ -3,9 +3,15 @@ using namespace std;
 int main(){
     int R,G,B,N;
     cin >> R >> G >> B >> N;
-    int r,g,b;
-    r = N / R;
-    g = N / G;
-    b = N / B;
+    int r,g;
+    int count = 0;
+    for(r=0;r*R<=N;r++){
+        for(g=0;g*G+r*R<=N;g++){
+            if((N - r * R - g * G)%B==0){
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
     return 0;
 }
